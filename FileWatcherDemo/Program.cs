ce3b43d73826d1fbad38392de385fdd7a07117d8
Ken Ling <ken88ling@gmail.com>
2019-03-08 16:05:10 +0800

    Main Method add Service configuration

--- FileWatcherDemo/Program.cs
+++ FileWatcherDemo/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Linq;
+using FileWatcherDemo.Extension;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace FileWatcherDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<FileWatcherService>();
+                });
+
+            if (isService)
+            {
+                await builder.RunAsServiceAsync();
+            }
+            else
+            {
+                await builder.RunConsoleAsync();
+            }
         }
     }
 }
