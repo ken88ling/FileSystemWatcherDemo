5569524630febc79ca71b8fc477b90f4246317a8
Ken Ling <ken88ling@gmail.com>
2019-03-08 15:37:59 +0800

    Add project files.

---
+++ FileWatcherDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileWatcherDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
